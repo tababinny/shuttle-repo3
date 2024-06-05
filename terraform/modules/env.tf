4dca88ea8005520350636f046765eb317f257cfd
tababinny <saiba.binny@outlook.com>
2024-06-05 19:05:51 +0530

    Update env.tf

--- terraform/modules/env.tf
+++ terraform/modules/env.tf
@@ -17,11 +17,13 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   kind = "Linux"
   reserved=true
 
+
   sku {
     tier = "Standard"
     size = "S1"
+}
 
-  }
+}
 
   resource "azurerm_app_service" "webapp" {
     name = var.webappname
@@ -30,5 +32,5 @@ resource "azurerm_app_service_plan" "appserviceplan" {
     app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
   }
 
-}
+
   
