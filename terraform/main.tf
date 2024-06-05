19d946ba6129994a7e1f23974e197d7d11881e04
tababinny <saiba.binny@outlook.com>
2024-06-05 19:45:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
     
   }
   client_id = "1a0a32de-3b2f-4790-aa90-5e14b8fb7187"
-  client_secret ="mxh8Q~x8tYMObvHd7HwfQ2GBePJTXLUiSJWj3auM"
+  client_secret =".5j8Q~ZMnElwru2DoOU-r5oiUJVGYgidEz~.JaWD"
   tenant_id = "31e46a46-0504-4e87-8878-5adea77cdfbe"
   subscription_id ="64db6a2c-2487-4a6f-91f2-ed6b12da00f6"
   
